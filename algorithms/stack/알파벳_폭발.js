8659e006a91251b98cae8d5aa5f2ff0a5cd1a0e7
sungseokmin <jkl154527@gmail.com>
2026-04-01 15:40:33 +0900

    refactor: remove unused service and associated configuration files

--- algorithms/stack/알파벳_폭발.js
+++ algorithms/stack/알파벳_폭발.js
@@ -1,38 +1,4 @@
-function solution(s) {
-  const stack = [];
-
-  for (const char of s) {
-    if (stack.length === 0) {
-      stack.push(char);
-      continue;
-    }
-
-    const stackTop = stack[stack.length - 1];
-    const isUpper = isUpperCase(stackTop);
-
-    if (isUpper) {
-      const toUpper = char.toUpperCase();
-      if (stackTop !== toUpper) {
-        stack.push(char);
-      } else {
-        stack.pop();
-      }
-    } else {
-      const toLower = char.toLowerCase();
-      if (stackTop !== toLower) {
-        stack.push(char);
-      } else {
-        stack.pop();
-      }
-    }
-  }
-
-  return stack.join("");
-}
-
-function isUpperCase(str) {
-  return str === str.toUpperCase();
-}
+function solution(s) {}
 
 console.log(solution("infFflearn"));
 console.log(solution("aAbBcC"));
--- algorithms/stack/택배_박스_정리_시스템.js
+++ algorithms/stack/택배_박스_정리_시스템.js
@@ -1,16 +1,4 @@
-function solution(boxes) {
-  const stack = [];
-
-  for (const box of boxes) {
-    const top = stack[stack.length - 1];
-
-    if (top !== box) {
-      stack.push(box);
-    }
-  }
-
-  return stack;
-}
+function solution(boxes) {}
 
 console.log(solution([2, 2, 5, 5, 5, 1, 1]));
 console.log(solution([7, 7, 8, 8, 0, 0, 0]));
